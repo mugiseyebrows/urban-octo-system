35d15a26637229a59bee7662118a5c1d16fecfaa
Doronin Stanislav <mugisbrows@gmail.com>
2024-06-19 21:40:34 +0300

    second

--- main.bat
+++ main.bat
@@ -1,11 +1,10 @@
 @echo off
 rem This file is generated from main.pbat, all edits will be lost
-set PATH=C:\Miniconda3;C:\Miniconda3\Scripts;%USERPROFILE%\Miniconda3;%USERPROFILE%\Miniconda3\Scripts;%PATH%
-echo 1 %GITHUB_PATH%
-echo 2 ${{ github.path }}
-python -c "import os; print(os.path.realpath('.'))"
+echo 1 %GITHUB_ACTION_PATH%
+echo 2 ${{ github.action_path }}
 echo 3
 if exist C:\Temp goto b_end
 echo 4
 :b_end
+set
 echo 5
