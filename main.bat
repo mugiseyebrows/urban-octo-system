41ad71208a1655f8f7048259d627b283a0d518d1
Doronin Stanislav <mugisbrows@gmail.com>
2024-06-19 22:09:37 +0300

    test cache

--- main.bat
+++ main.bat
@@ -1,8 +1,13 @@
 @echo off
 rem This file is generated from main.pbat, all edits will be lost
-pushd C:\temp
-    echo %CD%
-    pushd %~dp0
-        echo %CD%
-    popd
+set PATH=C:\Program Files\CMake\bin;C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build;%PATH%
+if exist C:/myapp goto foo_end
+echo myapp not found
+call vcvars64.bat
+if not exist "build" mkdir "build"
+pushd build
+    cmake -G Ninja -DCMAKE_INSTALL_PREFIX=C:/myapp ..
+    cmake --build .
+    cmake --install .
 popd
+:foo_end
