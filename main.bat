aa79a2d4e3d4b3c294f5d3910ff970ceac298de1
Doronin Stanislav <mugisbrows@gmail.com>
2024-06-19 21:49:26 +0300

    fourth

--- main.bat
+++ main.bat
@@ -1,10 +1,8 @@
 @echo off
 rem This file is generated from main.pbat, all edits will be lost
-echo 1 %GITHUB_WORKSPACE%
-echo 2 ${{ github.workspace }}
-echo 3
-if exist C:\Temp goto b_end
-echo 4
-:b_end
-set
-echo 5
+pushd C:\temp
+    echo %CD%
+    pushd %~dp0
+        echo %CD%
+    popd
+popd
