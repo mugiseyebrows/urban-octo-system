167fbeb26c1c7c9e82d46ba18b0a9bdac44e7a6d
Doronin Stanislav <mugisbrows@gmail.com>
2024-06-23 20:36:59 +0300

    rmdir

--- main.bat
+++ main.bat
@@ -8,12 +8,6 @@ if not defined CURL (
 echo CURL not found
 exit /b
 )
-where libpq.dll || echo ok
-where libmysql.dll || echo ok
-where gcc || echo ok
-where qmake || echo ok
-where ninja || echo ok
-where cmake || echo ok
 if exist C:\mingw1120_64\bin\gcc.exe goto mingw1120_end
 pushd %~dp0
     if not exist x86_64-11.2.0-release-posix-seh-rt_v9-rev3.7z "%CURL%" -L -o x86_64-11.2.0-release-posix-seh-rt_v9-rev3.7z https://github.com/cristianadam/mingw-builds/releases/download/v11.2.0-rev3/x86_64-11.2.0-release-posix-seh-rt_v9-rev3.7z
@@ -21,9 +15,15 @@ pushd %~dp0
     move C:\mingw64 C:\mingw1120_64
 popd
 :mingw1120_end
+where gcc || echo gcc not found
+where qmake || echo qmake not found
+where cmake || echo cmake not found
+where ninja || echo ninja not found
+where libpq.dll || echo libpq.dll not found
+where libmysql.dll || echo libmysql.dll not found
 if exist C:\Qt\6.7.1\mingw1120_64\bin\qmake.exe goto qt671_end
-rmdir /q /s C:\Program Files\PostgreSQL || echo ok
-rmdir /q /s C:\Program Files\MySQL || echo ok
+rmdir /s /q "C:\Program Files\PostgreSQL" || echo 1 > NUL
+rmdir /s /q "C:\Program Files\MySQL" || echo 1 > NUL
 pushd %~dp0
     if not exist qtbase-everywhere-src-6.7.1.zip "%CURL%" -L -o qtbase-everywhere-src-6.7.1.zip https://download.qt.io/official_releases/qt/6.7/6.7.1/submodules/qtbase-everywhere-src-6.7.1.zip
     if not exist qtbase-everywhere-src-6.7.1 7z x -y qtbase-everywhere-src-6.7.1.zip
@@ -37,12 +37,20 @@ pushd %~dp0
     popd
 popd
 :qt671_end
+if exist C:\qt\6.7.1\mingw1120_64\plugins\sqldrivers\qsqlmysql.dll goto mysql820_end
 if exist C:\mysql-8.2.0-winx64\bin\mysql.exe goto mysql820_end
 pushd %~dp0
     if not exist mysql-8.2.0-winx64.zip "%CURL%" -L -o mysql-8.2.0-winx64.zip https://cdn.mysql.com/Downloads/MySQL-8.2/mysql-8.2.0-winx64.zip
     7z x -y -oC:\ mysql-8.2.0-winx64.zip
 popd
 :mysql820_end
+if exist C:\qt\6.7.1\mingw1120_64\plugins\sqldrivers\qsqlmysql.dll goto qsqlmysql671_end
+where gcc || echo gcc not found
+where qmake || echo qmake not found
+where cmake || echo cmake not found
+where ninja || echo ninja not found
+where libpq.dll || echo libpq.dll not found
+where libmysql.dll || echo libmysql.dll not found
 pushd %~dp0
     if not exist "build-qsqlmysql" mkdir "build-qsqlmysql"
     if not exist qtbase-everywhere-src-6.7.1.zip "%CURL%" -L -o qtbase-everywhere-src-6.7.1.zip https://download.qt.io/official_releases/qt/6.7/6.7.1/submodules/qtbase-everywhere-src-6.7.1.zip
@@ -56,6 +64,7 @@ pushd %~dp0
         copy /y C:\mysql-8.2.0-winx64\bin\libcrypto-3-x64.dll C:\Qt\6.7.1\mingw1120_64\bin
     popd
 popd
+:qsqlmysql671_end
 if exist C:\postgresql-14\bin\psql.exe goto psql1412_end
 if exist C:\qt\6.7.1\mingw1120_64\plugins\sqldrivers\qsqlpsql.dll goto psql1412_end
 pushd %~dp0
@@ -65,6 +74,12 @@ pushd %~dp0
 popd
 :psql1412_end
 if exist C:\qt\6.7.1\mingw1120_64\plugins\sqldrivers\qsqlpsql.dll goto qsqlpsql671_end
+where gcc || echo gcc not found
+where qmake || echo qmake not found
+where cmake || echo cmake not found
+where ninja || echo ninja not found
+where libpq.dll || echo libpq.dll not found
+where libmysql.dll || echo libmysql.dll not found
 set PostgreSQL_ROOT=C:\postgresql-14
 pushd %~dp0
     if not exist "build-qsqlpsql" mkdir "build-qsqlpsql"
