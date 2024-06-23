eb7cc1c65c6b6134e23692fabbe795b59cfae160
Doronin Stanislav <mugisbrows@gmail.com>
2024-06-23 18:31:16 +0300

    build qt6 with sql plugins

--- main.bat
+++ main.bat
@@ -1,14 +1,85 @@
 @echo off
 rem This file is generated from main.pbat, all edits will be lost
-set PATH=C:\Program Files\CMake\bin;C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build;%PATH%
-if exist C:/myapp goto foo_end
-echo myapp not found
-call vcvars64.bat
-if not exist "build" mkdir "build"
-pushd build
-    cmake -G Ninja -DCMAKE_INSTALL_PREFIX=C:/myapp ..
-    cmake --build .
-    cmake --install .
+set PATH=C:\mingw1120_64\bin;C:\Program Files\7-Zip;C:\Program Files\Meson;C:\Program Files\CMake\bin;C:\postgresql-14\bin;C:\qt\6.7.1\mingw1120_64\bin;C:\mysql-8.2.0-winx64\lib;C:\mysql-8.2.0-winx64\bin;%PATH%
+if exist "C:\Program Files\Git\mingw32\bin\curl.exe" set CURL=C:\Program Files\Git\mingw32\bin\curl.exe
+if exist "C:\Program Files\Git\mingw64\bin\curl.exe" set CURL=C:\Program Files\Git\mingw64\bin\curl.exe
+if exist "C:\Windows\System32\curl.exe" set CURL=C:\Windows\System32\curl.exe
+if not defined CURL (
+echo CURL not found
+exit /b
+)
+if exist "C:\Program Files\Git\usr\bin\patch.exe" set PATCH=C:\Program Files\Git\usr\bin\patch.exe
+if not defined PATCH (
+echo PATCH not found
+exit /b
+)
+if exist C:\mingw1120_64\bin\gcc.exe goto mingw1120_end
+pushd %~dp0
+    if not exist x86_64-11.2.0-release-posix-seh-rt_v9-rev3.7z "%CURL%" -L -o x86_64-11.2.0-release-posix-seh-rt_v9-rev3.7z https://github.com/cristianadam/mingw-builds/releases/download/v11.2.0-rev3/x86_64-11.2.0-release-posix-seh-rt_v9-rev3.7z
+    7z x -y -oC:\ x86_64-11.2.0-release-posix-seh-rt_v9-rev3.7z
+    move C:\mingw64 C:\mingw1120_64
 popd
-:foo_end
-dir C:\myapp
+:mingw1120_end
+if exist C:\Qt\6.7.1\mingw1120_64\bin\qmake.exe goto qt671_end
+rmdir /q /s C:\Program Files\PostgreSQL || echo ok
+pushd %~dp0
+    if not exist qtbase-everywhere-src-6.7.1.zip "%CURL%" -L -o qtbase-everywhere-src-6.7.1.zip https://download.qt.io/official_releases/qt/6.7/6.7.1/submodules/qtbase-everywhere-src-6.7.1.zip
+    if not exist qtbase-everywhere-src-6.7.1 7z x -y qtbase-everywhere-src-6.7.1.zip
+    pushd qtbase-everywhere-src-6.7.1
+        "%PATCH%" -p1 -i ..\qfilesystemmodel.patch
+        cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=C:/Qt/6.7.1/mingw1120_64 -DQT_QMAKE_TARGET_MKSPEC=win32-g++ -DQT_BUILD_TESTS=FALSE -DQT_BUILD_EXAMPLES=FALSE -DFEATURE_system_zlib=OFF .
+        ninja
+        ninja install
+        copy /y "C:\mingw1120_64\bin\libstdc++-6.dll" C:\Qt\6.7.1\mingw1120_64\bin
+        copy /y C:\mingw1120_64\bin\libgcc_s_seh-1.dll C:\Qt\6.7.1\mingw1120_64\bin
+        copy /y C:\mingw1120_64\bin\libwinpthread-1.dll C:\Qt\6.7.1\mingw1120_64\bin
+    popd
+popd
+:qt671_end
+if exist C:\mysql-8.2.0-winx64\bin\mysql.exe goto mysql820_end
+pushd %~dp0
+    if not exist mysql-8.2.0-winx64.zip "%CURL%" -L -o mysql-8.2.0-winx64.zip https://cdn.mysql.com/Downloads/MySQL-8.2/mysql-8.2.0-winx64.zip
+    7z x -y -oC:\ mysql-8.2.0-winx64.zip
+popd
+:mysql820_end
+pushd %~dp0
+    if not exist "build-qsqlmysql" mkdir "build-qsqlmysql"
+    if not exist qtbase-everywhere-src-6.7.1.zip "%CURL%" -L -o qtbase-everywhere-src-6.7.1.zip https://download.qt.io/official_releases/qt/6.7/6.7.1/submodules/qtbase-everywhere-src-6.7.1.zip
+    if not exist qtbase-everywhere-src-6.7.1 7z x -y qtbase-everywhere-src-6.7.1.zip
+    pushd build-qsqlmysql
+        cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="C:\Qt\6.7.1\mingw1120_64" -DMySQL_INCLUDE_DIR="C:\mysql-8.2.0-winx64\include" -DMySQL_LIBRARY="C:\mysql-8.2.0-winx64\lib\libmysql.lib" ..\qtbase-everywhere-src-6.7.1\src\plugins\sqldrivers
+        cmake --build .
+        cmake --install .
+        copy /y C:\mysql-8.2.0-winx64\lib\libmysql.dll C:\Qt\6.7.1\mingw1120_64\bin
+        copy /y C:\mysql-8.2.0-winx64\bin\libssl-3-x64.dll C:\Qt\6.7.1\mingw1120_64\bin
+        copy /y C:\mysql-8.2.0-winx64\bin\libcrypto-3-x64.dll C:\Qt\6.7.1\mingw1120_64\bin
+    popd
+popd
+if exist C:\postgresql-14\bin\psql.exe goto psql1412_end
+if exist C:\qt\6.7.1\mingw1120_64\plugins\sqldrivers\qsqlpsql.dll goto psql1412_end
+pushd %~dp0
+    if not exist postgresql-14.12-2-windows-x64-binaries.zip "%CURL%" -L -o postgresql-14.12-2-windows-x64-binaries.zip https://get.enterprisedb.com/postgresql/postgresql-14.12-2-windows-x64-binaries.zip
+    7z x -y -oC:\ postgresql-14.12-2-windows-x64-binaries.zip
+    move C:\pgsql C:\postgresql-14
+popd
+:psql1412_end
+if exist C:\qt\6.7.1\mingw1120_64\plugins\sqldrivers\qsqlpsql.dll goto qsqlpsql671_end
+set PostgreSQL_ROOT=C:\postgresql-14
+pushd %~dp0
+    if not exist "build-qsqlpsql" mkdir "build-qsqlpsql"
+    if not exist qtbase-everywhere-src-6.7.1.zip "%CURL%" -L -o qtbase-everywhere-src-6.7.1.zip https://download.qt.io/official_releases/qt/6.7/6.7.1/submodules/qtbase-everywhere-src-6.7.1.zip
+    if not exist qtbase-everywhere-src-6.7.1 7z x -y qtbase-everywhere-src-6.7.1.zip
+    pushd build-qsqlpsql
+        cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="C:\Qt\6.7.1\mingw1120_64" ..\qtbase-everywhere-src-6.7.1\src\plugins\sqldrivers
+        cmake --build .
+        cmake --install .
+        copy /y C:\postgresql-14\bin\libpq.dll C:\Qt\6.7.1\mingw1120_64\bin
+        copy /y C:\postgresql-14\bin\libssl-1_1-x64.dll C:\Qt\6.7.1\mingw1120_64\bin
+        copy /y C:\postgresql-14\bin\libcrypto-1_1-x64.dll C:\Qt\6.7.1\mingw1120_64\bin
+        copy /y C:\postgresql-14\bin\libintl-9.dll C:\Qt\6.7.1\mingw1120_64\bin
+        copy /y C:\postgresql-14\bin\libiconv-2.dll C:\Qt\6.7.1\mingw1120_64\bin
+    popd
+popd
+:qsqlpsql671_end
+gcc -dumpversion
+qmake -v
