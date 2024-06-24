4d43309c185b9eebaf9a1fb02ada2ec3d4c02c59
Doronin Stanislav <mugisbrows@gmail.com>
2024-06-24 15:04:29 +0300

    qwt

--- main.bat
+++ main.bat
@@ -1,6 +1,6 @@
 @echo off
 rem This file is generated from main.pbat, all edits will be lost
-set PATH=C:\mingw1120_64\bin;C:\Program Files\7-Zip;C:\Program Files\Meson;C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja;C:\Program Files (x86)\Android\android-sdk\cmake\3.22.1\bin;C:\Program Files\CMake\bin;C:\postgresql-14\bin;C:\qt\6.7.1\mingw1120_64\bin;C:\mysql-8.2.0-winx64\lib;C:\mysql-8.2.0-winx64\bin;C:\Miniconda3;C:\Miniconda3\Scripts;%USERPROFILE%\Miniconda3;%USERPROFILE%\Miniconda3\Scripts;%PATH%
+set PATH=C:\mingw1120_64\bin;C:\Program Files\7-Zip;C:\Program Files\Meson;C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja;C:\Program Files (x86)\Android\android-sdk\cmake\3.22.1\bin;C:\Program Files\CMake\bin;C:\postgresql-14\bin;C:\qt\6.7.1\mingw1120_64\bin;C:\mysql-8.2.0-winx64\lib;C:\mysql-8.2.0-winx64\bin;C:\Program Files\Git\cmd;C:\Miniconda3;C:\Miniconda3\Scripts;%USERPROFILE%\Miniconda3;%USERPROFILE%\Miniconda3\Scripts;%PATH%
 if exist "C:\Program Files\Git\mingw32\bin\curl.exe" set CURL=C:\Program Files\Git\mingw32\bin\curl.exe
 if exist "C:\Program Files\Git\mingw64\bin\curl.exe" set CURL=C:\Program Files\Git\mingw64\bin\curl.exe
 if exist "C:\Windows\System32\curl.exe" set CURL=C:\Windows\System32\curl.exe
@@ -15,12 +15,6 @@ pushd %~dp0
     move C:\mingw64 C:\mingw1120_64
 popd
 :mingw1120_end
-where gcc || echo gcc not found
-where qmake || echo qmake not found
-where cmake || echo cmake not found
-where ninja || echo ninja not found
-where libpq.dll || echo libpq.dll not found
-where libmysql.dll || echo libmysql.dll not found
 if exist C:\Qt\6.7.1\mingw1120_64\bin\qmake.exe goto qt671_end
 pushd %~dp0
     if not exist qtbase-everywhere-src-6.7.1.zip "%CURL%" -L -o qtbase-everywhere-src-6.7.1.zip https://download.qt.io/official_releases/qt/6.7/6.7.1/submodules/qtbase-everywhere-src-6.7.1.zip
@@ -43,12 +37,6 @@ pushd %~dp0
 popd
 :mysql820_end
 if exist C:\qt\6.7.1\mingw1120_64\plugins\sqldrivers\qsqlmysql.dll goto qsqlmysql671_end
-where gcc || echo gcc not found
-where qmake || echo qmake not found
-where cmake || echo cmake not found
-where ninja || echo ninja not found
-where libpq.dll || echo libpq.dll not found
-where libmysql.dll || echo libmysql.dll not found
 pushd %~dp0
     if not exist "build-qsqlmysql" mkdir "build-qsqlmysql"
     if not exist qtbase-everywhere-src-6.7.1.zip "%CURL%" -L -o qtbase-everywhere-src-6.7.1.zip https://download.qt.io/official_releases/qt/6.7/6.7.1/submodules/qtbase-everywhere-src-6.7.1.zip
@@ -72,12 +60,6 @@ pushd %~dp0
 popd
 :psql1412_end
 if exist C:\qt\6.7.1\mingw1120_64\plugins\sqldrivers\qsqlpsql.dll goto qsqlpsql671_end
-where gcc || echo gcc not found
-where qmake || echo qmake not found
-where cmake || echo cmake not found
-where ninja || echo ninja not found
-where libpq.dll || echo libpq.dll not found
-where libmysql.dll || echo libmysql.dll not found
 set PostgreSQL_ROOT=C:\postgresql-14
 pushd %~dp0
     if not exist "build-qsqlpsql" mkdir "build-qsqlpsql"
@@ -95,6 +77,17 @@ pushd %~dp0
     popd
 popd
 :qsqlpsql671_end
+if not exist qwt (
+    git clone https://git.code.sf.net/p/qwt/git qwt
+    pushd qwt
+        git checkout qwt-6.3
+    popd
+)
+pushd qwt
+    qmake
+    mingw32-make -j4
+    mingw32-make install
+popd
 where mugideploy || pip install mugideploy
 qmake
 mingw32-make
