101e782f3554557394e4d1ca755d32f4ec53b69b
Doronin Stanislav <mugisbrows@gmail.com>
2024-07-14 13:55:06 +0300

    cache test

--- main.bat
+++ main.bat
@@ -1,4 +1,4 @@
 @echo off
 rem This file is generated from main.pbat, all edits will be lost
-if not exist date.txt date /t > date.txt
-type date.txt
+if not exist time.txt time /t > time.txt
+type time.txt
