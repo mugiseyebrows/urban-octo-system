e29893ef57d4fb7ad654d55faf978358ee7e31bd
Doronin Stanislav <mugisbrows@gmail.com>
2024-07-14 14:02:32 +0300

    generate

--- main.bat
+++ main.bat
@@ -1,4 +1,6 @@
 @echo off
 rem This file is generated from main.pbat, all edits will be lost
-if not exist time.txt time /t > time.txt
+if exist time.txt goto generate_end
+time /t > time.txt
+:generate_end
 type time.txt
