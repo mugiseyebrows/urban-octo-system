2bdfadf2f278379ad89ff99a18415a3e0c100410
Doronin Stanislav <mugisbrows@gmail.com>
2025-01-16 17:53:11 +0300

    test

--- main.bat
+++ main.bat
@@ -1,8 +1,12 @@
 @echo off
 rem This file is generated from main.pbat, all edits will be lost
-set PATH=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build;C:\Miniconda3;C:\Miniconda3\Scripts;%USERPROFILE%\Miniconda3;%USERPROFILE%\Miniconda3\Scripts;%PATH%
-where cmake
+set PATH=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build;%PATH%
+where cmake || echo no cmake
+where ninja || echo no ninja
+where gcc || echo no gcc
+where python || echo no python
 call vcvars64.bat
-where cmake
-pip install pyfindlib
-pyfind C:\Miniconda > conda.txt
+where cmake || echo no cmake
+where ninja || echo no ninja
+where gcc || echo no gcc
+where python || echo no python
