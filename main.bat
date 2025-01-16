c40bb35e3b10021166d1e6c7e6da73bceb011642
Doronin Stanislav <mugisbrows@gmail.com>
2025-01-16 18:00:54 +0300

    test

--- main.bat
+++ main.bat
@@ -5,8 +5,13 @@ where cmake || echo no cmake
 where ninja || echo no ninja
 where gcc || echo no gcc
 where python || echo no python
+where 7z || echo no 7z
 call vcvars64.bat
 where cmake || echo no cmake
 where ninja || echo no ninja
 where gcc || echo no gcc
 where python || echo no python
+where 7z || echo no 7z
+pyfind C:\hostedtoolcache\windows > hostedtoolcache.txt
+pyfind C:\ProgramData\Chocolatey > chocolatey.txt
+pyfind C:\Program Files\Microsoft Visual Studio > visual_studio.txt
